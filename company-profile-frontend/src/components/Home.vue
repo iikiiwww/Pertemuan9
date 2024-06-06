6c1711c3ca94f96125feb8f023cc94d807c7b272
Your Name~ <you@example.com>
2024-06-06 14:57:10 +0700

    halo

---
+++ company-profile-frontend/src/components/Home.vue
@@ -0,0 +1,12 @@
+vue
+<template>
+<div>
+<h1>Welcome to PT GIT Solution</h1>
+<p>Solusi Teknologi Terbaik untuk Bisnis Anda</p>
+</div>
+</template>
+<script>
+export default {
+name: 'Home'
+}
+</script>
